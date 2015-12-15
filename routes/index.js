3c986e029a6036fa13ae2b35fab6f101100237bc
keith Driver <keith@just-the-one.co.uk>
2015-12-15 21:47:07 +0000

    more1

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,22 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET Hello World page. */
+router.get('/helloworld', function(req, res) {
+    res.render('helloworld', { title: 'Hello, World!' });
+});
+
+
+/* GET times page. */
+router.get('/times', function(req, res) {
+    var db = req.db;
+    var collection = db.get('mbp');
+
+    collection.find({},{},function(e,docs){
+        res.render('times', {
+            "userlist" : docs
+        });
+    });
+});
+
 module.exports = router;
