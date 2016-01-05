5cd0bb9af61891f2b5aa7b07aa5aa9cb4b7878e0
Keith Driver <keith@silver.local>
2016-01-05 23:04:53 +0000

    reads from mongo and jade presents

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/test');
+var client = require('mongodb').MongoClient;
+
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var assert = require('assert');
+var ObjectId = require('mongodb').ObjectID;
+var url = 'mongodb://localhost:27017/test';
+var docs = [];
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -11,8 +15,51 @@ router.get('/helloworld', function(req, res) {
     res.render('helloworld', { title: 'Hello, World!' });
 });
 
+var docs=[];
 
-/* GET times page. */
+var findTimes = function(db, callback) {
+    var cursor = db.collection('mbp').find( ).sort({"_id":-1}).limit(10);
+   docs = [];
+    cursor.each(function(err,doc) {
+                assert.equal(err,null);
+                //process.stdout.write("mongoing\n");
+                if ( doc != null) {
+                docs.push(doc);
+                }
+                else
+                {
+                process.stdout.write("total docs read " + docs.length );
+                process.stdout.write("\nstopped mongoing\n");
+                process.stdout.write(JSON.stringify(docs[0],null,4));
+                    callback();
+                }
+              
+                
+                });
+};
+
+//   var cnt = db.collection('mbp').count();
+//    var cursor = db.collection('mbp').find();
+//process.stdout.write("mongoing count " + cnt  + "\n");
+//	console.log(cnt);
+
+/* do the mongo */
+router.get('/times', function(req, res) {
+    keith="driver";
+    req.client.connect(url,function(err,db){
+      assert.equal(err, null);
+      findTimes(db,function() {
+                        db.close();
+                    });
+	});
+    process.stdout.write("render it " + docs.length + "\n");
+	
+           res.render('times', { "time_list" : docs , "number" : 1} );
+
+//    res.render('times', { title: 'times' });
+});
+
+/* GET times page. 
 router.get('/times', function(req, res) {
     var db = req.db;
     var collection = db.get('mbp');
@@ -23,5 +70,6 @@ router.get('/times', function(req, res) {
         });
     });
 });
+*/
 
 module.exports = router;
