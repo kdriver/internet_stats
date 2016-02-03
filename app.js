44aec30315cd8012cab8018be519a792d1433f38
Keith Driver <keith@silver.local>
2016-02-03 21:07:37 +0000

    It works!!!

--- app.js
+++ app.js
@@ -23,10 +23,8 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, './public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
-console.log(app.path());
-console.log('keith');
 
 
 // Make our db accessible to our router
